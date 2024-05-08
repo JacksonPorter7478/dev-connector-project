a25930b9ca874a6d8ed4c63e4c0af15be61ce9fe
FullStackDev001 <jacksonporter7478@outlook.com>
2024-05-08 08:09:37 -0700

    this first commit

---
+++ client/build/precache-manifest.1d7edfd89f3a912e3140eab76bb5392f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a2c3b2b70fce782db2012d0cdc2c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a786bc3b99eef0f5764",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "de0bb3cb458afc34662a",
+    "url": "/static/js/2.c6045767.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.c6045767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a786bc3b99eef0f5764",
+    "url": "/static/js/main.6ce81dca.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "144fbbd3bd247fc65078fdf07db3782c",
+    "url": "/static/media/spinner.144fbbd3.gif"
+  }
+]);
